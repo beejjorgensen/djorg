71e673ec89ed42facc6b9b93c7ea5dcf49e3eadd
Beej Jorgensen <beej@lambdaschool.com>
2018-05-09 11:19:26 -0700

    add view, admin

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Bookmark
 
-# Create your views here.
+def index(request):
+	context = {}
+
+	# TODO: biz logic
+
+	return render(request, 'bookmarks/index.html', context)
